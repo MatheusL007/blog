03e28e7df7b382f026a88b61c6c1b899a7cf013a
MatheusL007 <130005026+MatheusL007@users.noreply.github.com>
2024-09-27 17:32:05 -0300

    Categor

--- resources/views/categoria/categoria_create.blade.php
+++ resources/views/categoria/categoria_create.blade.php
@@ -9,12 +9,13 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <form method='POST'action=" {{URL('/categoria')}}">
+
+                    @csrf
+
                     <div class="form-group">
                         <label for="exampleInputEmail1">Nome</label>
                         <input type="text" name="nom"class="form-control" placeholder="Digite seu nome:">
                       </div>
-                    <label for="fname">nome:</label><br>
-                    <input type="text" id="fname" name="nome" value="John"><br>
 
                     <input type="submit" value="Enviar">
 
